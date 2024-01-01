78c66668f7ef926d4cd58b650221f28bfc1fa8b6
naushadraien <naushadraien5@gmail.com>
2024-01-01 21:48:18 +0545

    chore:added seo optimization

--- online-store-task/components/product-image.tsx
+++ online-store-task/components/product-image.tsx
@@ -11,34 +11,46 @@ interface ProductImageProps {
 function ProductImage({ image, title, fill }: ProductImageProps) {
   const [loading, setLoading] = useState(true);
 
-  const commonClasses = `object-contain duration-700 ease-in-out group-hover:opacity-75 ${
-    loading ? "scale-110 blur-2xl grayscale" : "scale-100 blur-0 grayscale-0"
-  }`;
-
   return (
     <>
       {fill ? (
-        <div style={{ position: "relative", paddingTop: "100%" }}>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "0",
+            paddingTop: "100%",
+          }}
+        >
           <Image
             src={image}
             alt={title}
             layout="fill"
             objectFit="contain"
-            className={commonClasses}
+            loading="eager"
+            className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
+              loading
+                ? "scale-110 blur-2xl grayscale"
+                : "scale-100 blur-0 grayscale-0"
+            }`}
             onLoadingComplete={() => setLoading(false)}
           />
         </div>
       ) : (
-        <div style={{ width: "400px", height: "1000px", position: "relative" }}>
-          <Image
-            src={image}
-            alt={title}
-            layout="fill"
-            objectFit="contain"
-            className={commonClasses}
-            onLoadingComplete={() => setLoading(false)}
-          />
-        </div>
+        <Image
+          src={image}
+          alt={title}
+          width={400}
+          height={1000}
+          layout="responsive"
+          loading="eager"
+          className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
+            loading
+              ? "scale-110 blur-2xl grayscale"
+              : "scale-100 blur-0 grayscale-0"
+          }`}
+          onLoadingComplete={() => setLoading(false)}
+        />
       )}
     </>
   );
