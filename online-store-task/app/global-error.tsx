aef6649b12e3c759803a48fd0731fd1af882e863
naushadraien <naushadraien5@gmail.com>
2024-06-13 16:53:02 +0545

    feat: global error

---
+++ online-store-task/app/global-error.tsx
@@ -0,0 +1,18 @@
+"use client";
+//Note: global-error.tsx is only enabled in production. In development, our error overlay i.e error.tsx will show instead.
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
